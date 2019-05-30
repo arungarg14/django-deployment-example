1c009effa990d3cf549055845d85a95cbbb30ef1
arun garg <arungarg1414@gmail.com>
2019-05-30 05:46:14 -0400

    django

---
+++ learn_users/basicapp/urls.py
@@ -0,0 +1,9 @@
+from basicapp import views
+from django.conf.urls import url
+
+app_name='basicapp'
+
+urlpatterns= [
+        url(r'^register/',views.register,name='register'),
+        url(r'^user_login/',views.user_login,name='user_login'),
+]
